9ac8e3e23f3a174b2ea59a3ba8d80c80a330091c
db46rt00ors <97573825@qq.com>
2020-04-13 19:34:28 +0800

    add file

---
+++ src/views/cost/common/enactment-dialog/add.vue
@@ -0,0 +1,197 @@
+<template>
+  <div>
+    <a-modal title="新增" width="70%" v-model="visibleObj.addvisible">
+      <a-form-model
+        :label-col="labelCol"
+        ref="ruleForm"
+        :model="form"
+        :rules="rules"
+        :wrapper-col="wrapperCol"
+      >
+        <a-row>
+          <a-col :span="24">
+            <a-form-model-item label="所属楼盘" :labelCol="{span: 3}" :wrapperCol="{span: 21}">
+              <a-select>
+                <a-select-option value="shanghai">Zone one</a-select-option>
+                <a-select-option value="beijing">Zone two</a-select-option>
+              </a-select>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12">
+            <a-form-model-item label="费项编号" prop="ownercode">
+              <a-input v-model="form.ownercode"></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-model-item label="费项名称" prop="ownername">
+              <a-input v-model="form.ownername"></a-input>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12">
+            <a-form-model-item label="收费方式">
+              <a-select>
+                <a-select-option value="shanghai">Zone one</a-select-option>
+                <a-select-option value="beijing">Zone two</a-select-option>
+              </a-select>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-model-item label="收费周期(月)" placeholder="1">
+              <a-select disabled>
+                <a-select-option value="1">1</a-select-option>
+                <a-select-option value="2">2</a-select-option>
+              </a-select>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="12">
+            <a-form-model-item label="单价">
+              <a-input></a-input>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="12">
+            <a-form-model-item label="关联费项">
+              <a-select placeholder="please select your zone">
+                <a-select-option value="shanghai">Zone one</a-select-option>
+                <a-select-option value="beijing">Zone two</a-select-option>
+              </a-select>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="21" :offset="3">
+            <a-form-model-item label :labelCol="{span: 0}" :wrapperCol="{span: 24}">
+              <a-checkbox-group>
+                <a-col :span="12">
+                  <a-checkbox value="1" name="type">允许【生成台帐】时直接修改此费项单价</a-checkbox>
+                </a-col>
+                <a-col :span="12">
+                  <a-checkbox value="2" name="type">打印收据时隐藏单价、读数、用量</a-checkbox>
+                </a-col>
+              </a-checkbox-group>
+            </a-form-model-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="9">
+            <a-form-model-item label="滞纳金比率" :labelCol="{span: 8}" :wrapperCol="{span: 16}">
+              <a-input placeholder="Basic usage" />
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-model-item label="(‰)，超期" :labelCol="{span: 6}" :wrapperCol="{span: 18}">
+              <a-input placeholder="Basic usage" />
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="7">
+            <a-form-model-item label="天后收取" :colon="false"></a-form-model-item>
+          </a-col>
+        </a-row>
+        <a-row>
+          <a-col :span="6">
+            <a-form-model-item label="催缴提醒" :labelCol="{span: 12}" :wrapperCol="{span: 10}">
+              <a-radio-group v-model="form.resource">
+                <a-radio value="1">是</a-radio>
+                <a-radio value="0">否</a-radio>
+              </a-radio-group>
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="8">
+            <a-form-model-item label="从未收取的缴费限期前 " :colon="false" :labelCol="{span: 8}" :wrapperCol="{span: 16}">
+              <a-input placeholder="Basic usage" />
+            </a-form-model-item>
+          </a-col>
+          <a-col :span="10">
+            <a-form-model-item label="天发送短信/微信" :colon="false"></a-form-model-item>
+          </a-col>
+        </a-row>
+      </a-form-model>
+      <template slot="footer">
+        <a-button type="primary" icon="save" @click="saveandnew">保存并新建</a-button>&nbsp;
+        <a-button type="primary" icon="issues-close" @click="saveandclose">保存并关闭</a-button>&nbsp;
+        <a-button type="primary" icon="close-square" @click="handleCancel">关闭</a-button>
+      </template>
+    </a-modal>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            labelCol: { lg: { span: 6 }, sm: { span: 6 } },
+            wrapperCol: { lg: { span: 18 }, sm: { span: 18 } },
+            form: {
+                affiliatedcompany: [],
+                ownercode: '',
+                ownername: ''
+            },
+            rules: {
+                affiliatedcompany: [{ required: true, message: '所属公司必须填写', trigger: 'change' }],
+                ownercode: [{ required: true, message: '业主编码必须填写', trigger: 'blur' }],
+                ownername: [{ required: true, message: '业主姓名必须填写', trigger: 'blur' }]
+            }
+        }
+    },
+    props: {
+        visibleObj: {
+            type: Object,
+            default: () => {
+                return {
+                    addvisible: false
+                }
+            }
+        }
+    },
+    methods: {
+        showModal() {
+            this.visible = true
+        },
+        saveandnew() {
+            this.$refs.ruleForm.validate(valid => {
+                if (valid) {
+                    this.$notification.success({
+                        message: '提示',
+                        duration: 3,
+                        description: '保存成功，请新增下一条记录！！'
+                    })
+                } else {
+                    console.log('error submit!!')
+                    return false
+                }
+            })
+        },
+        saveandclose() {
+            this.$refs.ruleForm.validate(valid => {
+                if (valid) {
+                    this.visibleObj.addvisible = false
+                } else {
+                    console.log('error submit!!')
+                    return false
+                }
+            })
+        },
+        handleCancel(e) {
+            console.log(e)
+            this.visibleObj.addvisible = false
+        }
+    }
+}
+</script>
+
+<style lang='less' scoped>
+.add {
+    width: 600px;
+    height: 500px;
+}
+.ant-form-item {
+    margin-top: 5px;
+    margin-bottom: 0px;
+}
+/deep/ .ant-checkbox-group {
+    width: 100%;
+}
+</style>
--- src/views/cost/common/enactment.vue
+++ src/views/cost/common/enactment.vue
@@ -1,12 +1,297 @@
 <template>
   <div>
-    <h1>费项设定</h1>
+    <a-row>
+      <a-form-model :model="form" layout="inline">
+        <a-form-model-item label="选择楼盘">
+          <a-select v-model="form.region" placeholder="选择楼盘" class="select">
+            <a-select-option value="1">楼盘1</a-select-option>
+            <a-select-option value="2">楼盘2</a-select-option>
+            <a-select-option value="3">楼盘3</a-select-option>
+            <a-select-option value="4">楼盘4</a-select-option>
+          </a-select>
+        </a-form-model-item>
+        <a-form-model-item label="收费方式">
+          <a-select v-model="form.ownerType" placeholder="全部" class="select">
+            <a-select-option value="1">全部</a-select-option>
+            <a-select-option value="2">按住户</a-select-option>
+            <a-select-option value="3">按房屋面积</a-select-option>
+            <a-select-option value="4">按使用面积</a-select-option>
+            <a-select-option value="5">按阁楼面积</a-select-option>
+          </a-select>
+        </a-form-model-item>
+      </a-form-model>
+    </a-row>
+    <a-row class="setting">
+      <a-button type="primary" icon="plus-circle" @click="showAddModal">新增</a-button>
+    </a-row>
+    <a-row>
+      <a-table
+        :columns="columns"
+        :dataSource="data"
+        @change="paginationChange"
+        position="bottom"
+        :pagination="pagination"
+        :rowSelection="rowSelection"
+        :scroll="{ x: 1900, y: 450 }"
+      >
+      </a-table>
+    </a-row>
+    <add :visibleObj="visibleObj"></add>
   </div>
 </template>
 
 <script>
-export default {}
+import add from './enactment-dialog/add'
+import _ from 'lodash'
+const columns = [
+    {
+        align: 'center',
+        fixed: 'left',
+        title: '编号',
+        dataIndex: 'key',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.key - b.key
+    },
+    {
+        align: 'center',
+        title: '所属楼盘',
+        dataIndex: 'property'
+    },
+    {
+        align: 'center',
+        title: '费项编号',
+        dataIndex: 'feenumber',
+        defaultSortOrder: 'descend',
+        sorter: (a, b) => a.feenumber - b.feenumber
+    },
+    {
+        align: 'center',
+        title: '费项名称',
+        dataIndex: 'feename'
+    },
+    {
+        align: 'center',
+        title: '收费方式',
+        dataIndex: 'chargemethod'
+    },
+    {
+        align: 'center',
+        title: '单价',
+        dataIndex: 'price'
+    },
+    {
+        align: 'center',
+        title: '允许改价',
+        dataIndex: 'pricechanges'
+    },
+    {
+        align: 'center',
+        title: '阶梯收费',
+        dataIndex: 'laddercharge'
+    },
+    {
+        align: 'center',
+        title: '收费周期',
+        dataIndex: 'chargingcycle'
+    },
+    {
+        align: 'center',
+        title: '催交提醒',
+        dataIndex: 'reminder'
+    },
+    {
+        align: 'center',
+        title: '催交天数',
+        dataIndex: 'daystorush'
+    },
+    {
+        align: 'center',
+        title: '续费提醒',
+        dataIndex: 'renewalreminder'
+    },
+    {
+        align: 'center',
+        title: '续费天数',
+        dataIndex: 'renewaldays'
+    },
+    {
+        align: 'center',
+        title: '关联费项',
+        dataIndex: 'relatedfees'
+    },
+    {
+        align: 'center',
+        title: '滞纳金比率',
+        dataIndex: 'penalty'
+    },
+    {
+        align: 'center',
+        title: '超期天数',
+        dataIndex: 'dayspastdue'
+    },
+    {
+        align: 'center',
+        title: '隐藏打印',
+        dataIndex: 'hideprinting'
+    }
+]
+
+const data = []
+for (let i = 0; i < 200; i++) {
+    data.push({
+        key: i,
+        property: '中东首座',
+        feenumber: '0017',
+        feename: _.sample(
+            [
+                '阁楼采暖费',
+                '卫生费',
+                '车位费',
+                '停车管理费',
+                '水电费',
+                '物业费',
+                '污水处理费',
+                '车库管理费',
+                '二次排污费',
+                '电梯电费',
+                '其他费用',
+                '阶梯水费',
+                '取暖费'
+            ],
+            1
+        ),
+        chargemethod: _.sample(
+            [
+                '按住户',
+                '按房屋面积',
+                '自定义',
+                '按加建面积',
+                '按车位面积',
+                '按使用量',
+                '按车库面积',
+                '按储藏室面积',
+                '按阁楼面积',
+                '按使用面积'
+            ],
+            1
+        ),
+        price: _.random(1, 1000),
+        pricechanges: _.sample(['是', '否'], 1),
+        laddercharge: _.sample(['是', '否'], 1),
+        chargingcycle: 1,
+        reminder: _.sample(['是', '否'], 1),
+        daystorush: 0,
+        renewalreminder: _.sample(['是', '否'], 1),
+        renewaldays: 15,
+        relatedfees: _.sample(['电费', '水费'], 1),
+        penalty: 5,
+        dayspastdue: 7,
+        hideprinting: _.sample(['是', '否'], 1)
+    })
+}
+
+export default {
+    data() {
+        return {
+            visibleObj: {
+                addvisible: false,
+                importexcelvisible: false
+            },
+            form: {
+                housecode: '',
+                region: undefined,
+                retrievingcontent: '',
+                ownerType: undefined
+            },
+            pagination: {
+                defaultPageSize: 10,
+                showTotal: total => `共 ${total} 条数据`,
+                showQuickJumper: true,
+                showSizeChanger: true,
+                pageSizeOptions: ['5', '10', '15', '20'],
+                onShowSizeChange: (current, pageSize) => (this.pageSize = pageSize)
+            },
+            data,
+            columns,
+            selectedRows: []
+        }
+    },
+    computed: {
+        rowSelection() {
+            return {
+                onChange: (selectedRowKeys, selectedRows) => {
+                    console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows)
+                },
+                onSelect: (record, selected, selectedRows) => {
+                    console.log(record, selected, selectedRows)
+                    // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+                    this.selectedRows = selectedRows
+                },
+                onSelectAll: (selected, selectedRows, changeRows) => {
+                    // eslint-disable-next-line vue/no-side-effects-in-computed-properties
+                    this.selectedRows = selectedRows
+                    console.log(selected, selectedRows, changeRows)
+                }
+            }
+        }
+    },
+    methods: {
+        paginationChange(pagination, filters, sorter) {
+            console.log('params', pagination, filters, sorter)
+        },
+        showAddModal() {
+            this.visibleObj.addvisible = true
+        },
+        showImportExcelModal() {
+            this.visibleObj.importexcelvisible = true
+        },
+        start() {
+            console.log(this.selectedRows)
+            this.$confirm({
+                title: '确认对话框',
+                content: `确认要处理选中的${this.selectedRows.length}项记录吗？`,
+                onOk() {
+                    return new Promise((resolve, reject) => {
+                        setTimeout(Math.random() > 0.5 ? resolve : reject, 1000)
+                    }).catch(() => console.log('Oops errors!'))
+                },
+                onCancel() {}
+            })
+        },
+        disable() {
+            this.$confirm({
+                title: '确认对话框',
+                content: `确认要处理选中的${this.selectedRows.length}项记录吗？`,
+                onOk() {
+                    return new Promise((resolve, reject) => {
+                        setTimeout(Math.random() > 0.5 ? resolve : reject, 1000)
+                    }).catch(() => console.log('Oops errors!'))
+                },
+                onCancel() {}
+            })
+        }
+    },
+    components: {
+        add
+    }
+}
 </script>
 
-<style>
+<style lang="less" scoped>
+.select {
+    width: 160px;
+}
+.retrievingcontent-icon {
+    position: absolute;
+    top: 50%;
+    right: 10px;
+    transform: translateY(-50%);
+}
+/deep/ input,
+/deep/ .ant-select-selection {
+    border-radius: 4px;
+}
+.setting {
+    margin: 10px 0;
+}
 </style>
