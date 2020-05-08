c1f7079464f31e51376c363428fdbde037ff2b06
db46rt00ors <97573825@qq.com>
2020-05-08 17:18:14 +0800

    滞纳金设置

---
+++ src/views/parkingmanagement/penalty.edit/edit.vue
@@ -0,0 +1,84 @@
+<template>
+  <div>
+    <a-modal
+      :title="`编辑-${penaltyobj.record === null ? '' : penaltyobj.record.property_name}`"
+      width="50%"
+      v-model="penaltyobj.visible"
+    >
+      <a-row>
+        <a-form :form="form" :labelCol="labelCol" :wrapperCol="wrapperCol">
+          <a-row>
+            <a-col :span="12">
+              <a-form-item label="滞纳金比率">
+                <a-input
+                  v-decorator="[
+                    'parking_fee_ratio',
+                    {
+                      rules: [{ required: true, message: 'Please input your note!' }],
+                      initialValue: penaltyobj.record === null ? '' : penaltyobj.record.parking_fee_ratio
+                    }
+                  ]"
+                />
+              </a-form-item>
+            </a-col>
+            <a-col :span="12">
+              <a-form-item label="超期天数">
+                <a-input
+                  v-decorator="[
+                    'expiry_date',
+                    {
+                      rules: [{ required: true, message: 'Please input your note!' }],
+                      initialValue: penaltyobj.record === null ? '' : penaltyobj.record.expiry_date
+                    }
+                  ]"
+                />
+              </a-form-item>
+            </a-col>
+          </a-row>
+        </a-form>
+      </a-row>
+    </a-modal>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        penaltyobj: {
+            type: Object,
+            default: () => {
+                return {
+                    visible: false
+                }
+            }
+        }
+    },
+    data() {
+        return {
+            form: this.$form.createForm(this),
+            labelCol: { span: 8 },
+            wrapperCol: { span: 12 }
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+.form-row {
+    /deep/.ant-form-item-label {
+        width: 7em;
+        text-align: right;
+    }
+    /deep/.ant-form-item-control-wrapper {
+        width: 70%;
+        /deep/.ant-calendar-picker {
+            width: 100%;
+        }
+    }
+}
+.ant-form-item {
+    margin-top: 0px;
+    margin-bottom: 0px;
+    width: 90%;
+}
+</style>
--- src/views/parkingmanagement/penalty.vue
+++ src/views/parkingmanagement/penalty.vue
@@ -1,13 +1,97 @@
 <template>
-  <h1>滞纳金管理</h1>
+  <a-row class="content">
+    <a-table
+      :columns="columns"
+      :dataSource="data"
+      @change="paginationChange"
+      position="bottom"
+      :pagination="pagination"
+      bordered
+      :scroll="{ y: 400}"
+    >
+      <a slot="property_code" slot-scope="text, record" @click="edit(record)">{{ text }}</a>
+    </a-table>
+    <penalty-edit :penaltyobj="penaltyobj"></penalty-edit>
+  </a-row>
 </template>
 
 <script>
-export default {
+import penaltyEdit from './penalty.edit/edit'
+const columns = [
+    {
+        width: '5%',
+        align: 'center',
+        title: '编号',
+        dataIndex: 'key',
+        key: 'key'
+    },
+    {
+        align: 'center',
+        width: '8%',
+        title: '楼盘编码',
+        dataIndex: 'property_code',
+        key: 'property_code',
+        scopedSlots: { customRender: 'property_code' }
+    },
+    { align: 'center', width: '26%', title: '楼盘名称', dataIndex: 'property_name', key: 'property_name' },
+    { align: 'center', title: '车位滞纳金比率(‰)', dataIndex: 'parking_fee_ratio', key: 'parking_fee_ratio' },
+    { align: 'center', title: '超期日期(天)', dataIndex: 'expiry_date', key: 'expiry_date' }
+]
 
+const data = [
+    {
+        key: '1',
+        property_code: 'A01',
+        property_name: '中东首座',
+        parking_fee_ratio: '2.00',
+        expiry_date: '15'
+    },
+    {
+        key: '2',
+        property_code: 'B01',
+        property_name: '盈科首座',
+        parking_fee_ratio: '6.00',
+        expiry_date: '30'
+    }
+]
+export default {
+    data() {
+        return {
+            pagination: {
+                defaultPageSize: 10,
+                showTotal: total => `共 ${total} 条数据`,
+                showQuickJumper: true,
+                showSizeChanger: true,
+                pageSizeOptions: ['5', '10', '15', '20'],
+                onShowSizeChange: (current, pageSize) => (this.pageSize = pageSize)
+            },
+            data,
+            columns,
+            penaltyobj: {
+                visible: true,
+                record: null
+            }
+        }
+    },
+    methods: {
+        paginationChange(pagination, filters, sorter) {
+            console.log('params', pagination, filters, sorter)
+        },
+        edit(record) {
+            this.penaltyobj.visible = true
+            this.penaltyobj.record = record
+        }
+    },
+    components: {
+        penaltyEdit
+    }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.content {
+    min-height: 500px;
+    background-color: #fff;
+    padding: 20px;
+}
 </style>
